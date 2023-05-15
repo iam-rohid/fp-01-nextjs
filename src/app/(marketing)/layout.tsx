7ca71f8ac28cb86991ddf1eb36f0f7e51032409f
Rohid <rohidul209@gmail.com>
2023-05-15 12:22:23 +0600

    async error for header fix

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 export default function HomeLayout({ children }: PropsWithChildren) {
   return (
     <div className="relative flex min-h-screen flex-col">
+      {/* @ts-expect-error Server Component */}
       <Header />
       <div className="flex-1">{children}</div>
       <Footer />
